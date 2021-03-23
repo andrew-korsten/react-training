c7a52bf91d4b2f20ba8f323c516e38cf832347da
Nick <andrew052@mail.ru>
2021-03-23 18:02:03 +0300

    first commit

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,5 +1,5 @@
-// A5 Pass in the handleDelete function as a prop (A6 in Home)
-const BlogList = ({ blogs, title, handleDelete }) => {
+
+const BlogList = ({ blogs, title }) => {
     // const blogs = props.blogs;
     // const title = props.title;
 
@@ -17,9 +17,6 @@ const BlogList = ({ blogs, title, handleDelete }) => {
                 <div className="blog-preview" key={blog.id} >
                     <h2>{ blog.title }</h2>
                     <p>Written by { blog.author }</p>
-                    {/* A1. Create the button for the deletion */}
-                    {/* A2. Create the two-layer AF because we will need to pass in the data into it without it being called here + pass in the blog.id from the map function run on the current blog (A3 is in Home)*/}
-                    <button className="delete-blog" onClick={() => handleDelete(blog.id)}>Delete the blog</button>
                 </div>
             ))}    
             
--- src/Home.js
+++ src/Home.js
@@ -1,3 +1,5 @@
+this is master
+
 import { useState } from "react";
 import BlogList from "./BlogList";
 import Test from "./Test";
@@ -9,18 +11,9 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ])
 
-  // A3. Create the handleDelete function here because this is where data is and we should always create the handling functions in the file where the data is, and then we should pass through the props to the consuming components.
-    const handleDelete = (id) => {
-        // A6. filter out the deleted blog, i.e. retain all the blogs whose id doesn't match tha passed-in id
-        const newBlogs = blogs.filter((blog) => blog.id != id);
-        // A7. Update the state with newBlogs
-        setBlogs(newBlogs);
-    }
-
     return (
         <div className="home">
-            {/* A4. Specify the function as the pro (A5 in BlogList)*/}
-            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+            <BlogList blogs={blogs} title="All Blogs" />
             <Test />
         </div>
     );
